a7c0f4124521a5d4a7f7ae5d89e83ebbb545a728
victor <55814035+m-vnio@users.noreply.github.com>
2024-04-01 18:11:16 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -16,3 +16,62 @@ $elements.range.addEventListener('input', ()=> {
 })
 
 $elements.image.scrollIntoView({ block: "center", inline: "center" })
+
+let move = false
+let coordenadaInicialX
+let coordenadaInicialY
+let scrollLeft
+let scrollTop
+
+$elements.container.addEventListener('mousedown', e => {
+    if (e.which === 1 || e.button === 0) {
+
+        move = true
+        
+        coordenadaInicialX = e.pageX - $elements.container.offsetLeft
+        coordenadaInicialY = e.pageY - $elements.container.offsetTop
+
+        scrollLeft  = $elements.container.scrollLeft
+        scrollTop   = $elements.container.scrollTop
+
+        $elements.container.setAttribute('style', 'overflow:hidden')
+    
+    }
+})
+
+$elements.container.addEventListener('mousemove', e => {
+    if( move ) {
+        const espaciadoX = e.pageX - $elements.container.offsetLeft
+        const distanciaX = espaciadoX - coordenadaInicialX
+
+        const espaciadoY = e.pageY - $elements.container.offsetTop
+        const distanciaY = espaciadoY - coordenadaInicialY
+        
+        $elements.container.scrollLeft = scrollLeft - distanciaX
+        $elements.container.scrollTop = scrollTop - distanciaY
+    }
+})
+
+$elements.container.addEventListener('mouseup', e => {
+    move = false
+    $elements.container.setAttribute('style', '')
+})
+
+
+const customEventInput = new CustomEvent('input')
+
+$elements.container.addEventListener('wheel', e => {
+    if( move ) {
+        if( e.deltaY > 0 ) {
+            $elements.range.value = parseInt( $elements.range.value ) - 50
+            
+        } else {
+            $elements.range.value = parseInt( $elements.range.value ) + 50
+        }
+    
+        $elements.range.dispatchEvent( customEventInput )
+    }
+})
+
+
+addEventListener('contextmenu', e => e.preventDefault())
